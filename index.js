2102f1ddc77e5e75d7a794191d268566146766fa
Gino Ochoa <gochoa1088@gmail.com>
2022-10-18 10:44:03 -0700

    post endpoint, 201 and 400

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ const { v4: uuid } = require("uuid");
 
 const app = express();
 
-//deines a new endpoint
+// allows for json support
+app.use(express.json());
+
+// defines a new GET endpoint
 app.get("/outfit", (req, res) => {
   const tops = ["Black", "White", "Orange", "Navy"];
   const jeans = ["Grey", "Dark Grey", "Black", "Navy"];
   const shoes = ["Black", "Grey", "Black", "Brown"];
 
+  // returns the data
   res.json({
     top: _.sample(tops),
     jeans: _.sample(jeans),
     shoes: _.sample(shoes),
   });
+});
+
+// defines a new POST endpoint
+app.post("/comments", (req, res) => {
+  const id = uuid();
+  const content = req.body.content;
+
+  if (!content) {
+    // 400: Not found, bad request
+    return res.sendStatus(400);
+  }
+
+  // 201: object successfully created
+  res.sendStatus(201);
 });
 
 app.listen(3000, () => console.log("API Server is running..."));
