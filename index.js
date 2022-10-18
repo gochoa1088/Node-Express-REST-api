d5476824ff8a2c97e7c50b6b7266e9fa70e68c3f
Gino Ochoa <gochoa1088@gmail.com>
2022-10-18 10:35:40 -0700

    randomize outfits

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ const { v4: uuid } = require("uuid");
 
 const app = express();
 
+//deines a new endpoint
+app.get("/outfit", (req, res) => {
+  const tops = ["Black", "White", "Orange", "Navy"];
+  const jeans = ["Grey", "Dark Grey", "Black", "Navy"];
+  const shoes = ["Black", "Grey", "Black", "Brown"];
+
+  res.json({
+    top: _.sample(tops),
+    jeans: _.sample(jeans),
+    shoes: _.sample(shoes),
+  });
+});
+
 app.listen(3000, () => console.log("API Server is running..."));
